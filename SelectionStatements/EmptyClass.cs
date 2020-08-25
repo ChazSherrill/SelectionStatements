d59d85309792fc0a0acb7ed0fc31ee2aaede66c0
Chaz Sherrill <chazsherrill@icloud.com>
2020-08-25 11:47:47 -0500

    spacing succckkkss

---
+++ SelectionStatements/EmptyClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace SelectionStatements
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+            /*var r = new Random();
+               var faveNumber = r.Next(1, 1000);
+
+               int absDiff = 1;
+               Console.WriteLine("Guess my favorite number");
+               while (absDiff != 0)
+               {   
+               var userInput = int.Parse(Console.ReadLine());
+
+               int diff = userInput - faveNumber;
+
+               absDiff = Math.Abs(diff);
+
+                   if (absDiff == 0)
+                   {
+                       Console.WriteLine("You can read minds.");
+                   }
+                   else if (absDiff < 10)
+                   {
+                       Console.WriteLine("So close I can taste it");
+                   }
+                   else if (absDiff < 20)
+                   {
+                       Console.WriteLine("something something darkside");
+                   }
+                   else if (absDiff < 30)
+                   {
+                       Console.WriteLine("warmer");
+                   }
+                   else if (absDiff < 40)
+                   {
+                       Console.WriteLine("Palpatine Voice:good good");
+                   }
+                   else if (absDiff < 50)
+                   {
+                       Console.WriteLine("Alrigh Proffessor X chill out");
+                   }
+                   else if (absDiff < 100)
+                   {
+                       Console.WriteLine("pay attention scrub");
+                   }
+                   else
+                   {
+                       Console.WriteLine("guess again");
+
+                   }
+               }*/
+        }
+    }
+}
---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+
+        public static string YourSubject(string yourSubject)
+        {
+            switch (yourSubject.ToLower())
+            {
+                case "math": return "math is for nerds";
+                case "english": return "didn't you speak english at home";
+                case "science": return "ok dexter's laboratory";
+                case "history": return "what happened happened let it go";
+                case "coding": return "you have no girl friend";
+                case "music": return "band geek";
+                default: return "You're probably not learneded";
+            }
+        }
+            static void Main(string[] args)
+            {
+               
+                Console.WriteLine("Whtat's your favorite subject?");
+
+                string subject = Console.ReadLine();
+
+                Console.WriteLine(YourSubject(subject));
+
+
+            }
+        
+    }
+}
