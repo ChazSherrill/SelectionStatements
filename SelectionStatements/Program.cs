d3eb64a876766b29ef56a1d881f9fa63161d47f3
Chaz Sherrill <chazsherrill@icloud.com>
2020-08-25 13:00:48 -0500

    some updates

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -5,19 +5,8 @@ namespace SelectionStatements
     class Program
     {
 
-        public static string YourSubject(string yourSubject)
-        {
-            switch (yourSubject.ToLower())
-            {
-                case "math": return "math is for nerds";
-                case "english": return "didn't you speak english at home";
-                case "science": return "ok dexter's laboratory";
-                case "history": return "what happened happened let it go";
-                case "coding": return "you have no girl friend";
-                case "music": return "band geek";
-                default: return "You're probably not learneded";
-            }
-        }
+       
+        
             static void Main(string[] args)
             {
                
@@ -25,8 +14,17 @@ namespace SelectionStatements
 
                 string subject = Console.ReadLine();
 
-                Console.WriteLine(YourSubject(subject));
-
+              
+            switch (subject.ToLower())
+            {
+                case "math": Console.WriteLine("math is for nerds");break;
+                case "english": Console.WriteLine("didn't you speak english at home"); break;
+                case "science": Console.WriteLine("ok dexter's laboratory"); break;
+                case "history": Console.WriteLine("what happened happened let it go"); break;
+                case "coding": Console.WriteLine("you have no girl friend");break;
+                case "music": Console.WriteLine("band geek");break;
+                default: Console.WriteLine( "You're probably not learneded");  break;
+            }
 
             }
         
